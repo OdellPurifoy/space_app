2013db869a11bd7f0a9dad3cdcbba9c15d4986ac
Odell Purifoy <odell@visitdays.com>
2021-01-03 19:55:54 -0500

    Rspec tests created for model validations

--- app/models/moon.rb
+++ app/models/moon.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Moon < ApplicationRecord
   belongs_to :planet
+  validates :moon_name, :moon_surface_type, presence: true
+  validates :moon_surface_temperature, numericality: true
 end
--- spec/models/moon_spec.rb
+++ spec/models/moon_spec.rb
@@ -1,5 +1,38 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Moon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'validates presence of moon name' do
+    record = Moon.new
+    record.moon_name = ''
+    record.validate
+    expect(record.errors[:moon_name]).to include("can't be blank")
+
+    record.moon_name = 'Europa'
+    record.validate
+    expect(record.errors[:moon_name]).to_not include("can't be blank")
+  end
+
+  it 'validates presence of moon surface type' do
+    record = Moon.new
+    record.moon_surface_type = ''
+    record.validate
+    expect(record.errors[:moon_surface_type]).to include("can't be blank")
+
+    record.moon_surface_type = 'Gas'
+    record.validate
+    expect(record.errors[:moon_surface_type]).to_not include("can't be blank")
+  end
+
+  it 'validates surface temperature is a number' do
+    record = Moon.new
+    record.moon_surface_temperature = 'number'
+    record.validate
+    expect(record.errors[:moon_surface_temperature]).to include('is not a number')
+
+    record.moon_surface_temperature = 1000
+    record.validate
+    expect(record.errors[:moon_surface_temperature]).to_not include('is not a number')
+  end
 end
